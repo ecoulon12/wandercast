b8f13bbbd6472ac465281363e11cfedab8b11149
Drew Uramoto <duramoto@usc.edu>
2025-04-10 14:21:49 -0700

    Changed name, swap to ISR based read

--- weatherSensors.c
+++ weatherSensors.c
@@ -12,19 +12,13 @@ int main(void)
     TIMSK1 |= (1 << OCIE1A);    // Enable CTC interrupt
     sei();                      // Enable global interrupts
     OCR1A = 38400;              // Set the counter modulus (for 0.25hz)
-    TCCR1B |= (1 << CS10);      // Set prescaler for divide by 1024,
+    TCCR1B |= ((1 << CS10) | (1 << CS12));      // Set prescaler for divide by 1024,
                                 // also starts timer
     
+
+
     while (1) {
-        if(PINC & windTick)
-        {
-            windSpdRaw += 1;
-            windTick = 0;
-        }
-        else if (~PINC & ~windTick)
-        {
-            windTick = 1;
-        }
+
     }
 
     return 0;   
@@ -33,6 +27,19 @@ int main(void)
 
 ISR(TIMER1_COMPA_vect)
 {
-    windSpd = 0.6 * windSpdRaw; //ticks every 4 seconds, 2.4km windspeed @ 1 tick/sec
+    windSpd = 0.6 * windSpdRaw; //runs every 4 seconds, 2.4km windspeed @ 1 tick/sec
+}
+
+ISR(PCINT1_vect)
+{
+    if(PINC & windTick)
+    {
+        windSpdRaw += 1;
+        windTick = 0;
+    }
+    else if (~PINC & ~windTick)
+    {
+        windTick = 1;
+    }
 }
         
