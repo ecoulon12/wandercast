75e1c22ace4cbd6ed3c826825808dd2c2dcdb9ef
Drew Uramoto <duramoto@usc.edu>
2025-04-23 21:04:09 -0700

    Update weatherSensors.c

--- weatherSensors.c
+++ weatherSensors.c
@@ -6,6 +6,8 @@
 int windSpdRaw = 0;
 bool windTick = true; //required as the wind speed sensor is contact based, and the signal can vary in length based on wind speed
 float windSpd = 0;
+unsigned char windDirRaw
+char windDir;
 
 
 //N.B. Very unoptimized code, just trying to get something that works
@@ -39,7 +41,7 @@ ISR(PCINT1_vect)
     {
         windSpdRaw += 1;
         windTick = 0;
-        snprintf(printData, 32, "Wind Speed =%02x",windSpdRaw);
+        snprintf(printData, 32, "Wind Speed =%02x",windSpdRaw); // print function to test
         lcd_write_string(printData);
     }
     else if (~PINC & ~windTick)
@@ -47,11 +49,34 @@ ISR(PCINT1_vect)
         windTick = 1;
     }
 }
+
+void windVane(){ 
+    ADCSRA |= (1 << ADSC); // Starts ADC sample cycle
+    while((ADCSRA & 1) != 0){ // Waits for ADC sample to return
+    }
+     = ADCH; // Sample retrieved, stored in windDir
+    /*CALIBRATION VALUES
+    (V/5) * 255 = ADC
+    DIR|   V   | ADC
+    N  = 3.853 = 196.5 
+    NE = 2.277 = 116.1
+    E  = 0.465 = 23.7
+    SE = 0.915 = 46.7
+    S  = 1.425 = 72.7
+    SW = 3.097 = 158.0
+    W  = 4.621 = 235.7
+    NW = 4.341 = 221.4         */
+}
+
         
 
 void weather_kit_init(){
     PCICR |= (1 << PCIE1);  // Enable PCINT on Port C
     PCMSK1 |= (1 << PCINT10 | 1 << PCINT11); // Interrupt on PC2, PC3
+    ADMUX |= (1 << REFS0 | ~(1 << REFS1) | (1 << ADLAR) | 1 << MUX0); /*  -Sets ADC range from 0-5V (REFS register)
+                                                                        -Sets readings to 8-bit definition (ADLAR)
+                                                                        -Sets active read channel to PC1 (MUX register)*/
+    ADCSRA |= (1 << ADPS0 | 1 << ADPS1 | 1 << ADPS2 | 1 << ADEN); //Sets ADC clock scalar to 128 (ADPS register) and enables ADC (ADEN)
 }
 
 
